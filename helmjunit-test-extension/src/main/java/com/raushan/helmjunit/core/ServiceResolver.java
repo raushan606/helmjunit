ad38bdc968a220c9dca1955f60046c37f7754df5
Raushan <raushankumar606@gmail.com>
2025-06-16 20:05:10 +0530

    ©️ Added Copyright statement

--- helmjunit-test-extension/src/main/java/com/raushan/helmjunit/core/ServiceResolver.java
+++ helmjunit-test-extension/src/main/java/com/raushan/helmjunit/core/ServiceResolver.java
@@ -1,5 +1,20 @@
-package com.raushan.helmjunit.core;
+/*
+ * Copyright 2025 Raushan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
+package com.raushan.helmjunit.core;
 
 import java.util.Optional;
 
@@ -12,7 +27,7 @@ public interface ServiceResolver {
      * Finds the preferred service name for a given Helm release in a namespace.
      *
      * @param releaseName the Helm release name
-     * @param namespace the Kubernetes namespace
+     * @param namespace   the Kubernetes namespace
      * @return optional service name
      * @throws Exception on failure
      */
@@ -22,7 +37,7 @@ public interface ServiceResolver {
      * Retrieves the primary service port for a given service.
      *
      * @param serviceName the Kubernetes service name
-     * @param namespace the namespace
+     * @param namespace   the namespace
      * @return the port
      * @throws Exception on failure
      */
--- helmjunit-test-extension/src/main/java/com/raushan/helmjunit/dsl/HelmTestConsumer.java
+++ helmjunit-test-extension/src/main/java/com/raushan/helmjunit/dsl/HelmTestConsumer.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2025 Raushan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.raushan.helmjunit.dsl;
 
 /**
  * Interface for a consumer that accepts a HelmTestEnvironment.
  * This functional interface is used to define a method that takes a HelmTestEnvironment
  * and performs some operations on it, typically in the context of a Helm test.
+ *
  * @see HelmTestEnvironment
  */
 @FunctionalInterface
